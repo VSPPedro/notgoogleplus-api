12883df976cab0a560556ee546d567547ca20363
Pedro Paiva <pedrovsp109@gmail.com>
2018-05-06 17:05:07 -0300

    Correcting email field variable

--- authentication/models.py
+++ authentication/models.py
@@ -22,7 +22,7 @@ class AccountManager(BaseUserManager):
 
 
 class Account(AbstractBaseUser):
-    email = models.Email.Field(unique=True)
+    email = models.EmailField(unique=True)
     username = models.CharField(max_length=40, unique=True)
 
     first_name = models.CharField(max_length=40, blank=True)
