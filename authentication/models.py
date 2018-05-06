8aecd8094df533c61405bb33933170777df30b51
Pedro Paiva <pedrovsp109@gmail.com>
2018-05-06 16:44:18 -0300

    Adding the class AccountManager.

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users muset have a valid username.')
+
+        account = self.model(
+            email=self.normalize_email(email), username=kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
 
 
 class Account(AbstractBaseUser):
